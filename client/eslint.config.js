1ea3eaf9205aeb8727d8334a536cac3eaa385aca
TheRedRish <rune@roeddik.net>
2026-01-09 13:10:36 +0100

    Fixed linter frontend added ignore

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,14 +1,34 @@
+import prettier from "eslint-config-prettier";
+import svelte from "eslint-plugin-svelte";
 import js from "@eslint/js";
 import globals from "globals";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
 
-export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
-  },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
-]);
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
+export default defineConfig(
+  [
+    includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
+    {
+      files: ["**/*.{js,mjs,cjs}"],
+      plugins: { js },
+      extends: ["js/recommended"],
+      languageOptions: { globals: globals.browser },
+    },
+
+    {
+      files: ["**/*.css"],
+      plugins: { css },
+      language: "css/css",
+      extends: ["css/recommended"],
+      rules: {
+        "css/no-invalid-properties": "off",
+      },
+    },
+  ],
+  prettier,
+  ...svelte.configs.prettier
+);
